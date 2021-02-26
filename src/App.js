20bad629462b05f0b065174db34303b3d680887c
kabir02091999 <kabir.02091999@gmail.com>
2021-02-26 14:16:39 -0400

    cambiso

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Pokemones from './componentes/Pokemones';
 import {Provider} from 'react-redux'
 import generateStore from './redux/store'
 
+/* hay dos ramas multiples y main */
+
 function App() {
   
   const store = generateStore()
